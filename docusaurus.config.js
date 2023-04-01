1ab2b8cb59c9e455b903d7d460c9d43c3a4a5f6b
Irfaq Syed <irazasyed@users.noreply.github.com>
2023-04-02 03:36:07 +0530

    Update social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -270,8 +270,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/social-card.jpg',
 
       colorMode: {
         defaultMode: 'dark',
